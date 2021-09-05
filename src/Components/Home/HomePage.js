f10cde4d9cbd99ba74c01b795d8328880ccd8673
Akshit <61937872+homeboy445@users.noreply.github.com>
2021-09-05 20:36:09 +0530

    Updated Menu's UI

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -34,7 +34,7 @@ const LandingPage = ({ auth }) => {
         <div className="hp2-sub">
             <h1 id="title">Debatotron</h1>
             <div className="hp-btn">
-                <button onClick={()=>window.location.href="/Register"}>Register</button>
+                <button onClick={()=>window.location.href="/register"}>Register</button>
                 <button onClick={()=>window.location.href="/signin"}>LogIn</button>
             </div>
         </div>
--- src/Components/Menu/Navigation.js
+++ src/Components/Menu/Navigation.js
@@ -1,65 +1,80 @@
-import React,{useState,useEffect,useContext} from 'react';
-import { Link } from 'react-router-dom';
-import AuthContext from '../../Contexts/AuthContext';
-import {animated,useSpring} from 'react-spring';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../Contexts/AuthContext";
+import { animated, useSpring } from "react-spring";
 
-const Navigation=()=>{
-    const [on,turn]=useState(true);
-    const [user,change_user]=useState('random');
-    const Auth=useContext(AuthContext);
-    const ToggleIt=()=>{
-        turn(!on);
-    }
-    const {x1,x2,x3,x4,x5,evt}=useSpring({
-        x1:on?[0,0,0]:[-45,-9,6],
-        x2:on?1:0,
-        x3:on?[0,0,0]:[45,-8,-15],
-        x4:on?[100,0,0]:[0,1,0],
-        evt:on?"none":"all"
-    })
-    return (
-        <div>
-        <div class="Ham-menu" onClick={ToggleIt}>
-            <animated.div class="bar1"
-            style={{
-                transform:x1.interpolate((x,y,z)=>`translate(${y}px,${z}px) rotate(${x}deg)`),
-            }}></animated.div>
-            <animated.div class="bar2"
-            style={{
-                opacity:x2.interpolate(x=>x)
-            }}></animated.div>
-            <animated.div class="bar3"
-            style={{
-                transform:x3.interpolate((x,y,z)=>`translate(${y}px,${z}px) rotate(${x}deg)`)
-            }}></animated.div>
-        </div>
-        <animated.nav className="nav-menu" style={{
-            transform:x4.interpolate((x,y,z)=>`translate(${x}%,${z}%)`),
-            opacity:x4.interpolate((x,y,z)=>y),
-            pointerEvents:evt.interpolate(str=>str)
-        }}>
-            <ul>
-            <Link to="/">
-                <li onClick={ToggleIt}>Home</li>
-            </Link>
-            <Link to="/new">
-                <li onClick={ToggleIt}>New Deb</li>
-            </Link>
-            <Link to="/OngoingDebs">
-                <li onClick={ToggleIt}>OnGoing Debs</li>
-            </Link>
-            <Link to="/Inbox">
-                <li onClick={ToggleIt}>Inbox</li>
-            </Link>
-            <Link to={`/Profile/${Auth.userInfo[0].name}`}>
-                <li onClick={ToggleIt}>Profile</li>
-            </Link>
-            <Link to="/signout">
-                <li onClick={ToggleIt}>Sign-Out</li>
-            </Link>
-            </ul>
-        </animated.nav>
+const Navigation = () => {
+  const [on, turn] = useState(true);
+  const Auth = useContext(AuthContext);
+  const ToggleIt = () => {
+    turn(!on);
+  };
+  const { x1, x2, x3, x4, evt } = useSpring({
+    x1: on ? [0, 0, 0] : [-45, -3, 6],
+    x2: on ? 1 : 0,
+    x3: on ? [0, 0, 0] : [45, -2, -15],
+    x4: on ? [-100, 0, 0] : [0, 1, 0],
+    evt: on ? "none" : "all",
+  });
+  return (
+    <div>
+      <div className="Ham-menu" onClick={ToggleIt}>
+        <animated.div
+          className="bar1"
+          style={{
+            transform: x1.interpolate(
+              (x, y, z) => `translate(${y}px,${z}px) rotate(${x}deg)`
+            ),
+          }}
+        ></animated.div>
+        <animated.div
+          className="bar2"
+          style={{
+            opacity: x2.interpolate((x) => x),
+          }}
+        ></animated.div>
+        <animated.div
+          className="bar3"
+          style={{
+            transform: x3.interpolate(
+              (x, y, z) => `translate(${y}px,${z}px) rotate(${x}deg)`
+            ),
+          }}
+        ></animated.div>
+      </div>
+      <animated.nav
+        className="nav-menu"
+        style={{
+          transform: x4.interpolate((x, y, z) => `translate(${x}%,${z}%)`),
+          opacity: x4.interpolate((x, y, z) => y),
+          pointerEvents: evt.interpolate((str) => str),
+        }}
+      >
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+        <ul>
+          <Link to="/">
+            <li onClick={ToggleIt}>Home</li>
+          </Link>
+          <Link to="/new">
+            <li onClick={ToggleIt}>New Deb</li>
+          </Link>
+          <Link to="/OngoingDebs">
+            <li onClick={ToggleIt}>OnGoing Debs</li>
+          </Link>
+          <Link to="/Inbox">
+            <li onClick={ToggleIt}>Inbox</li>
+          </Link>
+          <Link to={`/Profile/${Auth.userInfo[0].name}`}>
+            <li onClick={ToggleIt}>Profile</li>
+          </Link>
+          <Link to="/signout">
+            <li onClick={ToggleIt}>Sign-Out</li>
+          </Link>
+        </ul>
+      </animated.nav>
     </div>
-    )
-}
+  );
+};
 export default Navigation;
